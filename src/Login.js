0502fa31bd957cb6ba80d983feb3e764b1fc9362
stephpileggi <stephpileggi@gmail.com>
2018-12-06 18:05:57 -0500

    updated likes

--- src/Login.js
+++ src/Login.js
@@ -1,26 +1,25 @@
 import React from 'react';
 
+
+
 const Login = props => {
-    function isDisabled() {
-        if (props.inventory === 0) {
-            return true;
-        }
-        return false;
-    }
-
+ 
+    console.log(props.userImg) 
     return (
-        <div>
+        <div className="wrapper">
+            {props.user && 
+            <button onClick={props.showForm}>Submit Topic</button>}
 
-            <h2>{props.title}</h2>
-            <p>{props.inventory}</p>
-            <button
-                value={props.id}
-                onClick={props.updateInventory}
-                disabled={isDisabled()}>
-                Buy a Donut
-            </button>
+            {props.user ?
+                <button onClick={props.logout}>Log Out</button>
+                :
+                <button onClick={props.login}>Log In</button>
+            }
+            {props.user && <img className="userImage" src={props.userImg} alt=""/>
+            }
         </div>
     );
 };
+
 
 export default Login;
