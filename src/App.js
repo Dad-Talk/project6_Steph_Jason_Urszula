b6425c4ca17a2a82aaa24be812767af241d62206
Urszula <urszulad26@gmail.com>
2018-12-12 07:51:01 -0500

    wednesday morning commit

--- src/App.js
+++ src/App.js
@@ -76,35 +76,26 @@ class App extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
 
+    // When user submits form, create object that records values of user submission
+    const newSubmission = {
+      title: this.state.title,
+      description: this.state.description,
+      mood: this.state.newMood,
+      likes: 1,
+      userArray: ["test", "test2"]
+    }
 
-     if(this.state.newMood === "" && this.state.mood === "") {
-              // alert message 
-          } else {
+    // Push new object to store to firebase
+    posts.push(newSubmission); 
 
-            // When user submits form, create object that records values of user submission
-            const newSubmission = {
-              title: this.state.title,
-              description: this.state.description,
-              mood: this.state.newMood,
-              likes: 1,
-              userArray: ["test", "test2"]
-            }
-        
-            // Push new object to store to firebase
-            posts.push(newSubmission); 
-        
-            // Reset values of state
-            this.setState({
-              title: "",
-              description: "",
-              mood: "",
-              like: 1,
-              newMood: ""
-            });
-
-          }
-        
-
+    // Reset values of state
+    this.setState({
+      title: "",
+      description: "",
+      mood: "",
+      like: 1,
+      newMood: ""
+    });
   }
 
   //ON click of mood button, we need to change mood: newmood
@@ -119,10 +110,6 @@ class App extends Component {
 
     let filteredMoodArray = newNewMoodArray.filter((v, i, a) => a.indexOf(v) === i); 
     moodRef.update(filteredMoodArray);
-
-    // this.setState({
-    //   // newMood:""
-    // })
 
     console.log("newNewMoodArray", newNewMoodArray);
   }
@@ -241,7 +228,7 @@ login = () => {
           <div className="wrapper">
             {/* Submission form will only appear if showForm state is true, and user state is true */}
             {this.state.showForm && this.state.user && <SubmissionsForm handleChange={this.handleChange // Submission form
-                } handleSubmit={this.handleSubmit} title={this.state.title} description={this.state.description} newMoodSubmit={this.newMoodSubmit} moodArray={this.state.moodArray} newMood={this.state.newMood} mood={this.state.mood}/>} 
+                } handleSubmit={this.handleSubmit} title={this.state.title} description={this.state.description} newMoodSubmit={this.newMoodSubmit} moodArray={this.state.moodArray} newMood={this.state.newMood}/>}
             {/* Categories/moods */}
             <div className="moods-container clearfix">
               {Object.entries(this.state.moodArray).map(
